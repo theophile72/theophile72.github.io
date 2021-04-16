ef4cab9841b59ef0152624c87b5aeaf4a99f81db
theophile72 <81265376+theophile72@users.noreply.github.com>
2021-04-17 00:16:53 +0200

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,273 @@
+//Croquis
+//Image Click
+
+
+//Image Click
+document.getElementById('image1').addEventListener('click', function() {
+	if(document.getElementById("image1").style.width == "600px") {
+	    document.getElementById("image1").style.width ="200px";
+        document.getElementById("image1").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image1").style.width ="600px";
+        document.getElementById("image1").style.height ="600px";
+        document.getElementById("image1").style.filter ="grayscale(0%)";
+
+
+   }
+});
+//Image Click
+document.getElementById('image2').addEventListener('click', function() {
+	if(document.getElementById("image2").style.width == "800px") {
+	    document.getElementById("image2").style.width ="200px";
+        document.getElementById("image2").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image2").style.width ="800px";
+        document.getElementById("image2").style.height ="600px";
+        document.getElementById("image2").style.filter ="grayscale(0%)";
+   }
+});
+//Image Click
+document.getElementById('image3').addEventListener('click', function() {
+	if(document.getElementById("image3").style.width == "800px") {
+	    document.getElementById("image3").style.width ="200px";
+        document.getElementById("image3").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image3").style.width ="800px";
+        document.getElementById("image3").style.height ="600px";
+        document.getElementById("image3").style.filter ="grayscale(0%)";
+   }
+});
+
+//Image Click
+document.getElementById('image4').addEventListener('click', function() {
+	if(document.getElementById("image4").style.width == "800px") {
+	    document.getElementById("image4").style.width ="200px";
+        document.getElementById("image4").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image4").style.width ="800px";
+        document.getElementById("image4").style.height ="600px";
+        document.getElementById("image4").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image5').addEventListener('click', function() {
+	if(document.getElementById("image5").style.width == "800px") {
+	    document.getElementById("image5").style.width ="200px";
+        document.getElementById("image5").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image5").style.width ="800px";
+        document.getElementById("image5").style.height ="600px";
+        document.getElementById("image5").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image6').addEventListener('click', function() {
+	if(document.getElementById("image6").style.width == "800px") {
+	    document.getElementById("image6").style.width ="200px";
+        document.getElementById("image6").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image6").style.width ="800px";
+        document.getElementById("image6").style.height ="600px";
+        document.getElementById("image6").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image7').addEventListener('click', function() {
+	if(document.getElementById("image7").style.width == "800px") {
+	    document.getElementById("image7").style.width ="200px";
+        document.getElementById("image7").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image7").style.width ="800px";
+        document.getElementById("image7").style.height ="600px";
+        document.getElementById("image7").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image8').addEventListener('click', function() {
+	if(document.getElementById("image8").style.width == "800px") {
+	    document.getElementById("image8").style.width ="200px";
+        document.getElementById("image8").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image8").style.width ="800px";
+        document.getElementById("image8").style.height ="600px";
+        document.getElementById("image8").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image9').addEventListener('click', function() {
+	if(document.getElementById("image9").style.width == "800px") {
+	    document.getElementById("image9").style.width ="200px";
+        document.getElementById("image9").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image9").style.width ="800px";
+        document.getElementById("image9").style.height ="600px";
+        document.getElementById("image9").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image10').addEventListener('click', function() {
+	if(document.getElementById("image10").style.width == "800px") {
+	    document.getElementById("image10").style.width ="200px";
+        document.getElementById("image10").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image10").style.width ="800px";
+        document.getElementById("image10").style.height ="600px";
+        document.getElementById("image10").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image11').addEventListener('click', function() {
+	if(document.getElementById("image11").style.width == "800px") {
+	    document.getElementById("image11").style.width ="200px";
+        document.getElementById("image11").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image11").style.width ="800px";
+        document.getElementById("image11").style.height ="600px";
+        document.getElementById("image11").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image12').addEventListener('click', function() {
+	if(document.getElementById("image12").style.width == "800px") {
+	    document.getElementById("image12").style.width ="200px";
+        document.getElementById("image12").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image12").style.width ="800px";
+        document.getElementById("image12").style.height ="600px";
+        document.getElementById("image12").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image13').addEventListener('click', function() {
+	if(document.getElementById("image13").style.width == "800px") {
+	    document.getElementById("image13").style.width ="200px";
+        document.getElementById("image13").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image13").style.width ="800px";
+        document.getElementById("image13").style.height ="600px";
+        document.getElementById("image13").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image14').addEventListener('click', function() {
+	if(document.getElementById("image14").style.width == "800px") {
+	    document.getElementById("image14").style.width ="200px";
+        document.getElementById("image14").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image14").style.width ="800px";
+        document.getElementById("image14").style.height ="600px";
+        document.getElementById("image14").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image15').addEventListener('click', function() {
+	if(document.getElementById("image15").style.width == "800px") {
+	    document.getElementById("image15").style.width ="200px";
+        document.getElementById("image15").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image15").style.width ="800px";
+        document.getElementById("image15").style.height ="600px";
+        document.getElementById("image15").style.filter ="grayscale(0%)";
+
+   }
+});
+
+//Image Click
+document.getElementById('image16').addEventListener('click', function() {
+	if(document.getElementById("image16").style.width == "800px") {
+	    document.getElementById("image16").style.width ="200px";
+        document.getElementById("image16").style.height ="200px";
+
+
+	}
+ 
+    else{
+	    document.getElementById("image16").style.width ="800px";
+        document.getElementById("image16").style.height ="600px";
+        document.getElementById("image16").style.filter ="grayscale(0%)";
+
+   }
+
+});
